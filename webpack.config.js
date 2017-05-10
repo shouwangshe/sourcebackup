996fe0bd9de13b56344754e22a04eee797afd696
shouwangshe <525780689@qq.com>
2017-05-10 11:00:01 +0800

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,52 @@
+//webpack的配置文件
+//导入一个path模块
 const path = require('path');
+//引入webpack
+const webpack = require('webpack');
+//引入OpenBrowserPlugin插件
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+//导出模块
 module.exports = {
-    entry : path.join(__dirname,'src/main.js'),
-    output : {
-        path : path.join(__dirname,'dist'),
-        filename : 'bundle.js'
-    },
-    devServer : {
-        port : 8888,
-        hot : true,
-        inline : true
-    },
-    module : {
-        rules : [
-            {
-                test : /\.js$/,
-                loader : 'babel-loader',
-                exclude : /node_modules/,
-                query : {
-                    presets : ['es2015','stage-2']
-                }
-            }
-        ]
-    }
+	//入口文件
+	entry : path.join(__dirname,'src/main.js'),
+	//出口文件
+	output : {
+		//路径
+		path :  path.join(__dirname, 'dist') ,
+		//文件名
+		filename : 'bundle.js'
+	},
+	//webpack-dev-server的配置
+	devServer : {
+		port : 8888,
+		hot : true,
+		inline : true
+	},
+	//loader配置
+	module : {
+		rules : [
+			//css-loader的配置
+			{
+				test : /\.css$/,
+				//webpack 1.x的写法，还是兼容的
+				loader : "style-loader!css-loader"
+				//webpack 2.x的写法
+				// use : ["css-loader","style-loader"]
+				// 'exclude' : /node_modules/  //排除node_modules
+			},
+			//less-loader配置
+			{
+				test : /\.less$/,
+				loader : "style-loader!css-loader!less-loader",
+				exclude : /node_modules/  //排除node_modules
+			}
+		]
+	},
+
+	//插件的配置
+	plugins : [
+		//内置的BannerPlugin插件
+		new webpack.BannerPlugin('create by andy!!'),
+		new OpenBrowserPlugin({url : "http://localhost:8888"})
+	]
 }
